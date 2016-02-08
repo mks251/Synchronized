edd7fda35896193aef9dbb3f0e691edcfe77f017
User <User@192.168.0.102>
2016-02-08 11:55:47 +0600

    Thread synchronized

---
+++ Tutorel21/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+private static int number;
+	public static void main(String[] args) {
+	Thread t1 = new Thread(new Worker());
+	Thread t2 = new Thread(new Worker());
+	t1.start();
+	t2.start();
+	try {
+		t1.join();
+		t2.join();
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	System.out.println(number);
+
+	}
+public static synchronized void increceNumber(){
+	number++;
+}
+}
